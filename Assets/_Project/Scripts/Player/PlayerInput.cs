4ec7af638b6443f7c2ff5493b4f823d848aa4633
humbleGuy1 <vrudnevskiy@list.ru>
2023-06-11 21:04:17 +0400

    Add player input

---
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Player.InputService
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        private const string Vertical = "Vertical";
+        private const string Horizontal = "Horizontal";
+        private const string MouseY = "Mouse Y";
+        private const string MouseX = "Mouse X";
+
+        public float InputAxisY => Input.GetAxis(Vertical);
+        public float InputAxisX => Input.GetAxis(Horizontal);
+        public float InputMouseY => Input.GetAxis(MouseY);
+        public float InputMouseX => Input.GetAxis(MouseX);
+        public bool IsRunning => Input.GetKey(KeyCode.LeftShift);
+        public bool IsShooting => Input.GetKeyDown(KeyCode.Mouse0);
+    }
+}
+
---
+++ Assets/_Project/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,54 @@
+﻿using Player.InputService;
+using UnityEngine;
+
+namespace Player.Movement
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] private Camera _playerCamera;
+        [SerializeField] private float _walkSpeed = 1.15f;
+        [SerializeField] private float _runSpeed = 4.0f;
+        [SerializeField] private float _lookSpeed = 2.0f;
+        [SerializeField] private float _lookXLimit = 60.0f;
+        [SerializeField] private float _gravity = 150.0f;
+        [SerializeField] private PlayerInput _playerInput;
+
+        private CharacterController _characterController;
+        private Vector3 _moveDirection = Vector3.zero;
+        private float _rotationX = 0;
+        private bool _canMove = true;
+
+        private void Start()
+        {
+            _characterController = GetComponent<CharacterController>();
+
+            //Cursor.lockState = CursorLockMode.Locked;
+            //Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = _canMove ? (_playerInput.IsRunning ? _runSpeed : _walkSpeed) * _playerInput.InputAxisY : 0;
+            float curSpeedY = _canMove ? (_playerInput.IsRunning ? _runSpeed : _walkSpeed) * _playerInput.InputAxisX : 0;
+            _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (!_characterController.isGrounded)
+            {
+                _moveDirection.y -= _gravity * Time.deltaTime;
+            }
+
+            _characterController.Move(_moveDirection * Time.deltaTime);
+
+            if (_canMove)
+            {
+                _rotationX += -_playerInput.InputMouseY * _lookSpeed;
+                _rotationX = Mathf.Clamp(_rotationX, -_lookXLimit, _lookXLimit);
+                _playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, _playerInput.InputMouseX * _lookSpeed, 0);
+            }
+        }
+    }
+}
